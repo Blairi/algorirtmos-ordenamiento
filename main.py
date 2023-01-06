027c6fd97f39f336175693a4cde1d28977f55a08
Blairi <axel.avi.fer@gmail.com>
2023-01-06 12:32:10 -0600

    menu added

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 
-PATH = "/home/blairi/development/projects/sorting-algorithms/bubblesort/data/"
+PATH = "/home/blairi/development/projects/sorting-algorithms"
 
 
-def get_data(path:str) -> tuple[list[int], list[float]]:
+def get_data(path:str) -> tuple[list[int], list[int]]:
 
     file = open(path, "r")
 
@@ -19,31 +19,70 @@ def get_data(path:str) -> tuple[list[int], list[float]]:
         time = line.split(":")[1]
 
         ns.append( int(n) )
-        times.append( float(time) )
+        times.append( int(time) )
 
     file.close()
 
     return ( ns, times )
 
 
+def menu() -> None:
+    title = str
+    n_best, steps_best = (int, int)
+    n_worst, steps_worst = (int, int)
+    n_average, steps_average = (int, int)
+
+    while True:
+        print("Choose a sorting algorithm from the list typing the number")
+        print("0. Exit")
+        print("1. Bubble sort")
+        print("2. Merge sort")
+        opc = input(": ")
+        if not opc.isdigit():
+            print("Illegal choice.")
+            continue
+        
+        opc = int(opc)
+
+        if opc == 0: # Exit
+            break
+
+        if opc == 1: # Bubble sort
+            title = "Bubble sort"
+            n_best, steps_best = get_data(f"{PATH}/bubblesort/data/best_case.txt")
+            n_worst, steps_worst = get_data(f"{PATH}/bubblesort/data/worst_case.txt")
+            n_average, steps_average = get_data(f"{PATH}/bubblesort/data/average_case.txt")
+        
+        if opc == 2: # Merge sort
+            title = "Merge sort"
+            n_best, steps_best = get_data(f"{PATH}/mergesort/data/best_case.txt")
+            n_worst, steps_worst = get_data(f"{PATH}/mergesort/data/worst_case.txt")
+            n_average, steps_average = get_data(f"{PATH}/mergesort/data/average_case.txt")
+        
+        # Graph building...
+        fig, (ax0, ax1, ax2) = plt.subplots(nrows=1, ncols=3, figsize=(12, 6))
+        plt.suptitle(title, fontsize=16)
+
+        ax0.set_title("Worst case")
+        ax0.set_xlabel("Elements (n)")
+        ax0.set_ylabel("Number of steps")
+        ax0.plot(n_worst, steps_worst, marker = "*", color = "r")
+
+        ax1.set_title("Best case")
+        ax1.set_xlabel("Elements (n)")
+        ax1.set_ylabel("Number of steps")
+        ax1.plot(n_best, steps_best, marker = "*", color = "g")
+
+        ax2.set_title("Average case")
+        ax2.set_xlabel("Elements (n)")
+        ax2.set_ylabel("Number of steps")
+        ax2.plot(n_average, steps_average, marker = "*", color = "b")
+
+        plt.show()
+
+
 def main() -> None:
-
-    n_best, steps_best = get_data(f"{PATH}best_case.txt")
-    n_worst, steps_worst = get_data(f"{PATH}worst_case.txt")
-    n_average, steps_average = get_data(f"{PATH}average_case.txt")
-
-    fig, (ax0, ax1, ax2) = plt.subplots(nrows=1, ncols=3, figsize=(12, 6))
-
-    ax0.set_title("Worst case")
-    ax0.plot(n_worst, steps_worst, marker = "*", color = "r")
-
-    ax1.set_title("Best case")
-    ax1.plot(n_best, steps_best, marker = "*", color = "g")
-
-    ax2.set_title("Average case")
-    ax2.plot(n_average, steps_average, marker = "*", color = "b")
-
-    plt.show()
+    menu()
 
 
 main()
