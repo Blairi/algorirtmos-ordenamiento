2fb96cbf48148156c041bc0ab3d498941dfcef2e
Blairi <axel.avi.fer@gmail.com>
2023-01-07 18:41:37 -0600

    Implementation to count steps added

--- heapsort/heapsort.cpp
+++ heapsort/heapsort.cpp
@@ -5,19 +5,22 @@
 
 using namespace std;
 
-const int MAX = 10;
+const int MAX = 1000;
 
 int left(int i);
 int right(int i);
 void heapify(int arr[], int n, int i);
 void heap_sort(int arr[], int n);
+int heapify_steps(int arr[], int n, int i);
+int heap_sort_steps(int arr[], int n);
 
 int main(int argc, char const *argv[])
 {
     int *arr = generate_random_array(MAX);
-    print_array(arr, MAX);
-    heap_sort(arr, MAX);
-    print_array(arr, MAX);
+    // print_array(arr, MAX);
+    int steps = heap_sort_steps(arr, MAX);
+    // print_array(arr, MAX);
+    cout << "Steps: " << steps << endl;
     return 0;
 }
 
@@ -65,3 +68,50 @@ void heap_sort(int arr[], int n)
         heapify(arr, i, 0);
     }
 }
+
+
+int heapify_steps(int arr[], int n, int i)
+{
+    int steps = 0;
+    // Root of tree
+    int largest = i;
+    // Indexes of children
+    int L = left(i);
+    int R = right(i);
+
+    // Swapping the root
+    if (L < n && arr[L] > arr[largest])
+        largest = L;
+
+    if (R < n && arr[R] > arr[largest])
+        largest = R;
+
+    // If the largest changed, swap and make a recursive call for the sub-tree
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+        steps += heapify_steps(arr, n, largest);
+    }
+
+    steps ++;
+
+    return steps;
+}
+
+
+int heap_sort_steps(int arr[], int n)
+{
+    int steps = 0;
+    // Tree building
+    for (int i = n / 2 - 1; i >= 0; i--)
+        steps += heapify_steps(arr, n, i);
+ 
+    for (int i = n - 1; i > 0; i--) {
+        // Move as current root the first element
+        swap(arr[0], arr[i]);
+        // Call to heapify to reduced heap
+        steps += heapify_steps(arr, i, 0);
+    }
+    return steps;
+}
+
