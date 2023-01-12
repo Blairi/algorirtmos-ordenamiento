b495d8469f57fa38497da6769dd6f895e79d034d
Blairi <axel.avi.fer@gmail.com>
2023-01-12 11:47:56 -0600

    functions to count steps

--- quicksort/quicksort.cpp
+++ quicksort/quicksort.cpp
@@ -6,21 +6,26 @@
 using namespace std;
 
 
-const int MAX = 10;
+const int MAX = 1000;
 
 
 void func();
 int partition(int arr[], int low, int high);
 void quick_sort(int arr[], int low, int high);
+std::pair <int, int> partition_steps(int arr[], int low, int high);
+int quick_sort_steps(int arr[], int low, int high);
 
 
 int main(int argc, char const *argv[])
 {
     // func();
-    int *arr = generate_random_array(MAX);
+    int *arr = generate_sorted_array(MAX);
+    // int *arr = generate_random_array(MAX);
+    // int *arr = generate_inverted_array(MAX);
     print_array(arr, MAX);
-    quick_sort(arr, 0, MAX - 1);
+    int x = quick_sort_steps(arr, 0, MAX - 1);
     print_array(arr, MAX);
+    cout << "Steps: " << x << endl;
     return 0;
 }
 
@@ -53,6 +58,54 @@ void quick_sort(int arr[], int low, int high)
         quick_sort(arr, low, pi - 1);
         quick_sort(arr, pi + 1, high);
     }
+}
+
+
+std::pair <int, int> partition_steps(int arr[], int low, int high)
+{
+    int steps = 0;
+
+    int pivot = arr[high];
+    int i = low - 1;
+  
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+            steps ++;
+        }
+        steps ++;
+    }
+
+    swap(arr[i + 1], arr[high]);
+
+    // Returning the pivot and num. of steps as pair
+    return { i + 1, steps };
+}
+
+
+int quick_sort_steps(int arr[], int low, int high)
+{
+    int steps = 0;
+
+    if (low < high) {
+
+        std::pair <int, int> result = partition_steps(arr, low, high);
+
+        int pi = result.first;
+
+        int step = result.second;
+        steps += step;
+  
+        // Recursive calls separated by the pivot
+        steps += quick_sort_steps(arr, low, pi - 1);
+        steps += quick_sort_steps(arr, pi + 1, high);
+
+    }
+
+    steps ++;
+
+    return steps;
 }
 
 
